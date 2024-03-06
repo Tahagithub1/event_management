8b53ff04cd7de4ecdfff53db839bdda5f8afbf81
Tahagithub1 <mogevaraluffyy@gmail.com>
2024-03-06 17:49:39 +0330

    Job*

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('app:send-event-reminder')->daily();
+        $schedule->command('app:send-event-reminder')->everyMinute();
     }
 
     /**
